4c56df480b4d569dcf4e22723334e4e3ad9281c8
Lukas <lukas.jarosch@mail.com>
2019-07-11 17:45:28 +0200

    quick and dirty, v1

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/go-godin/mysql
+
+go 1.12
+
+require (
+	github.com/go-sql-driver/mysql v1.4.1 // indirect
+	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
+	github.com/jmoiron/sqlx v1.2.0 // indirect
+)
---
+++ mysql.go
@@ -0,0 +1,92 @@
+package mysql
+
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/golang-migrate/migrate"
+	"github.com/golang-migrate/migrate/database/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+type MySQL struct {
+	db   *sqlx.DB
+	dsn  string
+	opts *Options
+}
+
+const (
+	DefaultMigrationPath         = "migrations"
+	DefaultMaxOpenConnections    = 10
+	DefaultMaxIdleConnections    = 0
+	DefaultMaxConnectionLifetime = 600 * time.Second
+)
+
+// New will connect to the MySQL server using the given DSN
+func New(dsn string, options ...Option) (*MySQL, error) {
+	args := &Options{
+		MigrationPath:         DefaultMigrationPath,
+		MaxOpenConnections:    DefaultMaxOpenConnections,
+		MaxIdleConnections:    DefaultMaxIdleConnections,
+		MaxConnectionLifetime: DefaultMaxConnectionLifetime,
+	}
+
+	for _, opt := range options {
+		opt(args)
+	}
+
+	db, err := sqlx.Connect("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// configure connection pool
+	db.SetMaxOpenConns(args.MaxOpenConnections)
+	db.SetMaxIdleConns(args.MaxIdleConnections)
+	db.SetConnMaxLifetime(args.MaxConnectionLifetime)
+
+	return &MySQL{
+		db:   db,
+		dsn:  dsn,
+		opts: args,
+	}, nil
+}
+
+// Migrate to a specific version. It's assumed t
+func (m MySQL) Migrate(version uint) error {
+	db, err := sql.Open("mysql", m.dsn)
+	if err != nil {
+		return err
+	}
+	driver, err := mysql.WithInstance(db, &mysql.Config{})
+	migrations, err := migrate.NewWithDatabaseInstance(
+		fmt.Sprintf("file://%s", m.opts.MigrationPath),
+		"mysql",
+		driver)
+	if err != nil {
+		return err
+	}
+
+	err = migrations.Migrate(version)
+	if err != nil {
+		if strings.Contains(err.Error(), "no change") {
+			return nil
+		}
+		return err
+	}
+
+	return nil
+}
+
+// Close is just a proxy for convenient access to db.Close()
+func (m MySQL) Close() error {
+	return m.db.Close()
+}
+
+// DB is just a proxy for convenient access to the underlying sqlx implementation
+// This method is used a lot, therefore it's name is abbreviated.
+func (m MySQL) DB() *sqlx.DB {
+	return m.db
+}
